0184c50181e7f60f3be11f41475183d47e2fa795
Evergarasaracho <edoardo_vergara@hotmail.com>
2024-05-17 10:27:09 -0400

    actualicé los encabezados

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ hist_button = st.button("Construir histograma")
 scatter_button = st.button("Construir grafico de dispersión")
 
 if hist_button:
-    st.header('Lanzar una moneda') #Encabezado
+    st.header('histograma') #Encabezado
     st.write("construyendo un histograma") #Leyenda del histograma
     fig = px.histogram(data, x='odometer') #Creación del histograma
     st.plotly_chart(fig, use_container_width=True) #mostrar el histograma
 
 if scatter_button:
-    st.header("Lanzar una moneda") #Encabezado
+    st.header("scatter") #Encabezado
     st.write("Creando el grafico de dispersion") #leyenda del scatter
     fig2 = px.scatter(data, x="odometer", y="price") #creación del scatter
     st.plotly_chart(fig2, use_container_width=True) #mostrar el scatter
